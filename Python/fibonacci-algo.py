8c58494c7ba422e3e232c8fa84a250e5e85c80d2
riyaz-369 <riyazulislam0099@gmail.com>
2025-04-04 17:43:48 +0600

    fibonacci algo using recursion

--- Python/fibonacci-algo.py
+++ Python/fibonacci-algo.py
@@ -8,3 +8,12 @@ for fibo in range(18):
     print(newFibo)
     prev1 = prev2
     prev2 = newFibo
+
+# using recursion ------
+def F(n): 
+    if n <= 1:
+        return n;
+    else:
+        return F(n-1) + F(n-2);
+    
+print(F(10)) # 55
