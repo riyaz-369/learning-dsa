87ce9ddc112d19962d50f6fba1b1b2b39d1acca2
riyaz-369 <riyazulislam0099@gmail.com>
2025-04-04 17:44:17 +0600

    fibonacci algo using recursion

--- JS/fibonacci-algo.js
+++ JS/fibonacci-algo.js
@@ -9,3 +9,10 @@ for (let i = 0; i < 18; i++) {
   prev1 = prev2; // 1
   prev2 = newFibo; // 1
 }
+
+// using recursion ------
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(10)); // 55
